479cd2365e1e8c0c2c9fe6478c4d6970d088295e
Galvin <galvinleow@gmail.com>
2021-01-09 02:27:54 +0800

    GetOutfit endpoint

--- easywardrobe-backend/main.py
+++ easywardrobe-backend/main.py
@@ -30,7 +30,7 @@ app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
 # Use localhost for local database (with the default password set for your system).
 def connect_db():
     connection = psycopg2.connect(user="postgres",
-                                password="relaxaholics",
+                                password="01041996",
                                 host="localhost",
                                 port="5432",
                                 database="easywar")
@@ -311,12 +311,22 @@ def getOutfits():
 
         quote_select_query = """SELECT * FROM outfits"""
         cursor.execute(quote_select_query)
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Outfits successfully selected from outfits table")
-        print(cursor.fetchall())
-        
+        # connection.commit()
+        # count = cursor.rowcount
+        data = cursor.fetchall()
 
+        outfits = []
+        for outfits_raw in data:
+            outfit = {
+                "outfit_id": data[0],
+                "items" : data[1]
+            }
+            outfits.append(outfit)
+
+        print("Outfits successfully selected from outfits table")
+        
+        return {outfits}
+    
     except (Exception, psycopg2.Error) as error:
         if(connection):
             print("Error while selecting from outfits table", error)
