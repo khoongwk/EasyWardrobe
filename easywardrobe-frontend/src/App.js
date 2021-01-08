05b513f1707a020ab5a158dcc639fa292b130204
Wei Kit <weikit89@gmail.com>
2021-01-08 17:03:18 +0800

    Edit Routes

--- easywardrobe-frontend/src/App.js
+++ easywardrobe-frontend/src/App.js
@@ -2,8 +2,9 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
 import WardrobePage from './Components/WardrobePage';
-import OutfitsPage from './Components/OutfitsPage'
-import CustomAppBar from './Components/CustomAppBar'
+import OutfitsPage from './Components/OutfitsPage';
+import CustomAppBar from './Components/CustomAppBar';
+import Home from './Components/Home/Home';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 
@@ -14,9 +15,10 @@ class App extends Component {
         <BrowserRouter>
           <CustomAppBar />
           <Switch>
-            <Route exact path ='/' Component={Login} />
-            <Route exact path='/wardrobe' Component={WardrobePage}/>
-            <Route exact path='/outfits' Component={OutfitsPage} />
+            <Route exact path ='/' component={Login} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/wardrobe' component={WardrobePage}/>
+            <Route exact path='/outfits' component={OutfitsPage} />
           </Switch>
         </BrowserRouter>
       </Fragment>
